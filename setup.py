a8a7c1e419ed0849677b683bd0e25eb559701f22
jngaiii <jngai_@berkeley.edu>
2025-06-09 18:39:47 +0800

    another setup change

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     long_description=open("README.md").read(),  # Optionally include README.md as long description
     long_description_content_type="text/markdown",  # Specify README format
     url="https://github.com/J-Ngaiii/asuc-explore",  # Optional: Link to project repo
-    packages=[""],  # Let Python treat src as the package, leave this field blank
-    package_dir={"": "src"}, # Maps the package directly to src
+    packages=["src"],  # Let Python treat src as the package, leave this field blank
+    package_dir={"ASUCExplore": "src"} # Maps the package directly to src
     install_requires=[
         "numpy",      # numpy 1.26.4 is required for Python 3.12 compatibility
         "pandas",
