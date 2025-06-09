9b6565eb8bd6379e3bd9166672a9a8386370122d
jngaiii <jngai_@berkeley.edu>
2025-06-09 17:51:12 +0800

    added lowercase setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="asucexplore",  # Package name
+    version="1.0.0",  # Version of your package
+    author="Jonathan Ngai (ASUC)",  # Your name or organization
+    author_email="jngai_@berkeley.edu",  # Optional: Your email address
+    description="Tools for exploring and analyzing ASUC financial data.",  # Short description
+    long_description=open("README.md").read(),  # Optionally include README.md as long description
+    long_description_content_type="text/markdown",  # Specify README format
+    url="https://github.com/J-Ngaiii/asuc-explore",  # Optional: Link to project repo
+    packages=find_packages(),  # Automatically find all packages in the project
+    install_requires=[
+        "numpy",      # numpy 1.26.4 is required for Python 3.12 compatibility
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "spacy",
+        "scikit-learn",  # Ensure scikit-learn 1.5.1 is used (note: incompatible with Python 3.12)
+        "rapidfuzz"
+    ],
+    python_requires='>=3.11,<3.12', # generally use python 3.11.8
+)
